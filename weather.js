f882fd9191744ab2de0ec89e2e96742d58a6111a
cdavidson-ixxus <chris.davidson@ixxus.com>
2013-09-23 10:24:58 +0100

    Added Kube.css, changed location to tonbridge and cleaned up some backbone

--- weather.js
+++ weather.js
@@ -29,7 +29,7 @@ SimpleSample.Person = Backbone.Model.extend({});
 
 SimpleSample.WeatherData = Backbone.Model.extend({
 	url: function() {
-            return 'http://api.openweathermap.org/data/2.5/forecast?q=London,uk';
+            return 'http://api.openweathermap.org/data/2.5/forecast?q=tonbridge,uk&callback=?';
         }
 });
 
@@ -69,23 +69,6 @@ window.WineCollection = Backbone.Collection.extend({
 
 SimpleSample.App = function(){
     this.start = function(){
-        //var person = new SimpleSample.Person();
-        //var formView = new SimpleSample.FormView({model: person});
-        //var displayView = new SimpleSample.DisplayView({model: person});
-        //person.set({first_name: "joe", last_name: "bob"});
-        /*$.get("http://api.openweathermap.org/data/2.5/forecast?q=London,uk&callback=?", function(data){
-			success: function(data) {
-				alert("Success: ");
-			},
-			error: function(data) {
-				alert("Fail: ");
-			},
-			alert("Data: " + data);
-		});*/
-		
-		/*$.getJSON('http://openweathermap.org/data/2.1/find/city?lat=13.3428&lon=-6.2661&cnt=10&callback=?', function(data) { 
-			console.log(data); 
-		});*/
 		
 		$.getJSON('http://api.openweathermap.org/data/2.5/forecast?q=tonbridge,uk&callback=?', function(data) { 
 			workOutDay();
@@ -93,6 +76,7 @@ SimpleSample.App = function(){
 			for(var i = 0 ; i < data.list.length ; i++){
 				var date = new Date(data.list[i].dt*1000);
 				if(saturday.getDate() == date.getDate()){
+					//weather += '<div class="units-row">';
 					weather += date.toString() + "<br/>";
 					weather += "Wind " + data.list[i].wind.deg + " degrees " + (data.list[i].wind.speed*convertToMPH).toFixed(2) + "mph" + "<br/>";
 					for(var j = 0 ; j < data.list[i].weather.length ; j++){
@@ -102,6 +86,7 @@ SimpleSample.App = function(){
 						weather += "Rain " + data.list[i].rain['3h'] + "<br/>";
 					}
 					weather += "<hr/>";
+					//weather += '</div>';
 				}
 				
 			}
@@ -109,8 +94,8 @@ SimpleSample.App = function(){
 			//$('#weather').html(JSON.stringify(data,null , 2));
 		});
 		workOutDay();
-        
-        /*var weather = new SimpleSample.WeatherData();
+		
+		/*var weather = new SimpleSample.WeatherData();
         weather.fetch({
 			success: function(){
 				console.log(weather.models); // => 2 (collection have been populated)
@@ -121,7 +106,8 @@ SimpleSample.App = function(){
 				console.log(response);
 				console.log("Not in here");
 			}
-		});*/
+		});*/        
+        
     }
 }
     
